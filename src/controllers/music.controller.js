898a01c0fbf7f6d5a310ff9948f020fa9578b802
Tushar <tmr812151@gmail.com>
2026-02-20 12:06:18 +0530

    Added album field for musics

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,24 +1,9 @@
 const musicModel = require("../models/music.model");
+const albumModel = require("../models/album.model");
 const {uploadFile} = require("../service/storage.service");
 const jwt = require("jsonwebtoken");
 
 async function createMusic(req, res) {
-    
-    const token = req.cooikies.token ;
-    if(!token){
-        return res.status(401).json({ message: "Unauthorized"});
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if(decoded.role != "artitist"){
-            return res.status(403).json({ message: " You are not allowed to access the music"});
-        }
-
-        const {title} = req.body;
-        const file = req.file;
-
-        const result = await uploadFile(file.buffer.toString('base64'));
 
         const music = await musicModel.create({
             uri: result.url,
@@ -35,12 +20,43 @@ async function createMusic(req, res) {
                 artist: music.artist
             }
         })
-
-    } catch (error) {
-        console.error(error);
-        return res.status(401).json({ message: "Unauthorized"});
-    }
-
+ 
 }
 
-module.exports = {createMusic };
+async function createAlbum(params) {
+    try {
+        const token = req.cooikies.token;
+        if(!token) {
+            return res.status(401).json({message: "Unauthorized"});
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(decoded.role !== "artist" ){
+            return res.status(403).json({message: "You are not allowed to access the music"});
+        }
+
+        const { title, musicIds }= req.body;
+        const album = await albumModel.create({
+            title,
+            artist: decoded.id,
+            musics: musicIds
+        })
+
+        res.status(201).json({
+            message: "Album created successfully",
+            album: {
+                id: album._id,
+                title: album.title,
+                artist: album.artist,
+                musics: album.musics
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ message: "Unauthorized"})
+    } 
+    
+}
+
+module.exports = {createMusic, createAlbum };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+async function authArtist(req, res, next) {
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({message: "Uanuthorized"});
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(decoded.role !== "artist"){
+            res.status(403).json({message: "You are not allowed to create music "})
+        }
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({message: "Unauthorized"});   
+    }
+
+}
+
+module.exports = { authArtist };
